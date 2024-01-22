e4c726b775e7dbbb5d965248ed6e7c4a4176f698
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-22 00:19:10 -0600

    just kwesi portfolio: mobile nav started

--- src/app/ui/MobileNav.tsx
+++ src/app/ui/MobileNav.tsx
@@ -1,3 +1,32 @@
+'use client'
+
+import { Menu } from 'lucide-react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { Button } from '@/app/ui/button'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/app/ui/sheet'
+
 export function MobileNav() {
-  return <div className="sm:hidden">mobile nav</div>
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" className="sm:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetTitle>Icon</SheetTitle>
+      </SheetContent>
+    </Sheet>
+  )
 }
