160e000ff2085ac5c876d180dbf10d04625c72af
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-22 16:02:44 -0600

    mobile nav: done with

--- src/app/ui/MobileNav.tsx
+++ src/app/ui/MobileNav.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import * as React from 'react'
 import { Menu } from 'lucide-react'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Link, { LinkProps } from 'next/link'
+import { siteConfig } from '@/config/site'
+import { Icons } from '@/app/ui/icons'
 import { cn } from '@/lib/utils'
 import { Button } from '@/app/ui/button'
+import { usePathname } from 'next/navigation'
 import {
   Sheet,
   SheetClose,
@@ -17,16 +21,114 @@ import {
 } from '@/app/ui/sheet'
 
 export function MobileNav() {
+  const [open, setOpen] = React.useState(false)
+  const pathname = usePathname()
   return (
-    <Sheet>
-      <SheetTrigger>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
         <Button variant="ghost" className="sm:hidden">
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left">
-        <SheetTitle>Icon</SheetTitle>
+      <SheetContent side="left" className="pr-0">
+        <MobileLink
+          href="/"
+          className="flex items-center gap-3 ml-5"
+          onOpenChange={setOpen}
+        >
+          <Icons.logo className="h-4 w-4" />
+          <span className="font-bold">{siteConfig.name}</span>
+        </MobileLink>
+
+        <nav className="flex items-start gap-8 text-sm flex-col mt-10 ml-5">
+          <Link
+            href="/"
+            className={cn(
+              'transition-colors hover:text-foreground/80',
+              pathname === '/' ? 'text-foreground' : 'text-foreground/60'
+            )}
+            onClick={() => {
+              setOpen?.(false)
+            }}
+          >
+            Home
+          </Link>
+
+          <Link
+            href="/projects"
+            className={cn(
+              'transition-colors hover:text-foreground/80',
+              pathname.startsWith('/projects')
+                ? 'text-foreground'
+                : 'text-foreground/60'
+            )}
+            onClick={() => {
+              setOpen?.(false)
+            }}
+          >
+            Projects
+          </Link>
+
+          <Link
+            href="/work"
+            className={cn(
+              'transition-colors hover:text-foreground/80',
+              pathname.startsWith('/work')
+                ? 'text-foreground'
+                : 'text-foreground/60'
+            )}
+            onClick={() => {
+              setOpen?.(false)
+            }}
+          >
+            Work
+          </Link>
+
+          <Link
+            href="/contact-me"
+            className={cn(
+              'transition-colors hover:text-foreground/80',
+              pathname.startsWith('/contact-me')
+                ? 'text-foreground'
+                : 'text-foreground/60'
+            )}
+            onClick={() => {
+              setOpen?.(false)
+            }}
+          >
+            Contact Me
+          </Link>
+        </nav>
       </SheetContent>
     </Sheet>
+  )
+}
+
+interface MobileLinkProps extends LinkProps {
+  onOpenChange?: (open: boolean) => void
+  children: React.ReactNode
+  className?: string
+}
+
+function MobileLink({
+  href,
+  onOpenChange,
+  className,
+  children,
+  ...props
+}: MobileLinkProps) {
+  const router = useRouter()
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString())
+        onOpenChange?.(false)
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
+    </Link>
   )
 }
