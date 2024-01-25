d4e62c4d5812cb3c7d084fa405f30c24faa31fcc
Frederick Tetteh <40897279+just-kwesi@users.noreply.github.com>
2024-01-24 21:29:31 -0600

    icons fix (#9)
    * readme updated
    
    * metadata set for home page, opengraph and twitter set
    
    * metadata set for all pages
    
    * og and twitter image
    
    * metadata changes
    
    * manifest added
    
    * icons fixed
    
    * merge conflit for manifest.ts fixed

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,19 +4,19 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Frederick Tetteh's Portfolio Site",
     short_name: 'just-kwesi',
-    description: 'portfolio site for frederick tetttehg',
+    description: 'portfolio site for frederick tetteh',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
     icons: [
       {
-        src: '/android-chrome-192x192.png',
+        src: '/images/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: '/images/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
       },
