343d91a513e23d5edfef9fe526227744eb27db0d
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-24 19:21:12 -0600

    metadata set for home page, opengraph and twitter set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s | ${siteConfig.name}`,
   },
+  metadataBase: new URL(siteConfig.url),
   description: siteConfig.description,
   keywords: [
     'Frederick Tetteh',
@@ -33,11 +34,42 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: 'Frederick Tetteh', url: 'https://frederick-tetteh.com' }],
   creator: 'Frederick Tetteh',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: '@Fhred_rick',
+  },
   icons: {
     icon: '/favicon.ico',
-    // shortcut: '/favicon-16x16.png',
-    // apple: '/apple-touch-icon.png',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
   },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+}
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 }
 
 export default function RootLayout({
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,12 @@
 export const siteConfig = {
   name: 'Frederick Tetteh',
   url: 'https://portfolio-v2-two-rosy.vercel.app/',
-  ogImage: '',
+  ogImage: '/src/public/ogImage.png',
   description: 'Personal portfolio site for Frederick Tetteh',
+  links: {
+    twitter: 'https://twitter.com/Fhred_rick',
+    github: 'https://github.com/just-kwesi/portfolio-v2',
+  },
 }
 
 export type SiteConfig = typeof siteConfig
