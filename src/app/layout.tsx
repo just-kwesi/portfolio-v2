09c852b9259180c448c875ee84baa0df6ef3854c
Frederick Tetteh <kwesiossom@gmail.com>
2024-01-24 22:13:22 -0600

    twitter image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,19 +44,18 @@ export const metadata: Metadata = {
     siteName: siteConfig.name,
     images: [
       {
-        url: '/opengraph-image.png',
+        url: './opengraph-image.png',
         width: 1200,
         height: 630,
         alt: siteConfig.name,
       },
     ],
   },
-
   twitter: {
     card: 'summary_large_image',
     title: siteConfig.name,
     description: siteConfig.description,
-    images: ['/opengraph-image.png'],
+    images: ['./opengraph-image.png'],
     creator: '@Fhred_rick',
   },
   icons: {
